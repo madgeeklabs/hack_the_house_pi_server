4ecda8e913aca955e85315b67efd3e2f8cd93ec9
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2015-02-28 16:04:22 +0100

    request post id user

--- spawnNFC.js
+++ spawnNFC.js
@@ -1,23 +1,31 @@
 var exec = require('child_process').exec;
+var request = require('request');
 var child = exec('explorenfc-basic');
 
 child.stdout.on('data', function(data) {
 
 	var lines = data.toString().split('\n');
 
-    lines.forEach(function(line) {
-    	if (line.indexOf('{') != -1){
-    		console.log(line.substr(line.indexOf('{')));
-    		var info = JSON.parse(line.substr(line.indexOf('{')));
-    		console.log(info.type);
-    	}
-    });
+	lines.forEach(function(line) {
+		if (line.indexOf('{') != -1){
+			console.log(line.substr(line.indexOf('{')));
+			var info = JSON.parse(line.substr(line.indexOf('{')));
+			console.log(info.type);
+
+			request.post({
+				url: 'http://192.168.178.123:5006/lastId',
+				body: {
+					userId : info.id
+				}
+			});
+		}
+	});
 });
 
 child.stderr.on('data', function(data) {
-    console.log('stdout: ' + data);
+	console.log('stdout: ' + data);
 });
 
 child.on('close', function(code) {
-    console.log('closing code: ' + code);
+	console.log('closing code: ' + code);
 });
