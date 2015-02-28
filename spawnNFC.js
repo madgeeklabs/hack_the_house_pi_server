ffa3838d149853f11d6910c87671d27957eec5bd
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2015-02-28 14:59:58 +0100

    parse result

--- spawnNFC.js
+++ spawnNFC.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 var child = exec('explorenfc-basic');
 
 child.stdout.on('data', function(data) {
-	console.log(data.indexOf('Title'));
+	console.log(data.substr(data.indexOf('{'), data.indexOf('}'));
     console.log('stdout: ' + data);
 });
 
