803e8997ba0b5723aa86dcb48833ff07d4de8f21
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2015-02-28 15:23:52 +0100

    tostring

--- spawnNFC.js
+++ spawnNFC.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 var child = exec('explorenfc-basic');
 
 child.stdout.on('data', function(data) {
-	console.log(data);
+	data = data.toString();
 	console.log(data.substr(data.indexOf('{'), data.indexOf('}')), data.indexOf('}'));
 	var info = JSON.parse(data.substr(data.indexOf('{'), data.indexOf('}')));
 	console.log (info.type);
